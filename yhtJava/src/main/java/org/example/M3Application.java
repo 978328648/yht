f1f0fa9a74e8ea7bd9d633bc7c34cdd6ca3a405c
嘿嘿 <lyt01154983@cainiao.com>
2023-05-17 16:33:59 +0800

    feat: 测试链接

--- yhtJava/src/main/java/org/example/M3Application.java
+++ yhtJava/src/main/java/org/example/M3Application.java
@@ -11,7 +11,7 @@ public class M3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(M3Application.class, args);
-        System.out.println("启动成功...");
+        System.out.println("启动成功...1");
     }
 
 }
