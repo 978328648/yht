9aab70c3de243cbc95fac0952f2aec9ea594290c
lyt <978328648@qq.com>
2023-06-05 23:56:13 +0800

    1

--- yhtJava/.idea/sonarlint/issuestore/index.pb
+++ yhtJava/.idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
 N
 .idea/thub/project_setting.xml,5/9/59d03fef64fbf52e3c9a14867a605feef2fe79f3
-;
-yhtJava.iml,9/3/93c800b82eb9dcc651b4ea0bc0c5ee3a18230023
-w
-G.idea/sonarlint/issuestore/5/9/59d03fef64fbf52e3c9a14867a605feef2fe79f3,a/f/afcde7147d9c2635908ca621c6b21fc46108ab4f
--- yhtJava/src/main/java/org/example/M3Application.java
+++ yhtJava/src/main/java/org/example/M3Application.java
@@ -8,10 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages= "org.example")//添加扫包@ComponentScan(basePackages= "")
 @EnableAutoConfiguration
 public class M3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(M3Application.class, args);
-        System.out.println("启动成功...1");
+        System.out.println("启动成功...");
     }
 
 }
